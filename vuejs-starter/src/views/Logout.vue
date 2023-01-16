71ee3ce6a0d8b9ce4edbcb2cd463a2d47ce37f7b
Loan CLERIS <loan.cleris@gmail.com>
2023-01-16 22:39:16 +0100

    Add homepage
    - wip: router.path
    - add footer
    - add component article

---
+++ vuejs-starter/src/views/Logout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const router = useRouter();
+onMounted(() => {
+    store.dispatch('auth/logout')
+    router.push('/')
+})
+</script>
