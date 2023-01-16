c6d55cf0dfd0441347a08edf874b835d5414eb2e
Loan CLERIS <loan.cleris@gmail.com>
2023-01-16 13:01:58 +0100

    Add navbar & header
    - fix tailwindcss
    - adding navbar with feature menu & user
    - header (display)

---
+++ vuejs-starter/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { UserIcon, MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
+
+const router = useRouter()
+const navLink = [
+    { name: 'Les modèles', href: '/model', current: router.hasRoute('model') },
+    { name: 'Achat', href: '/achat', current: router.hasRoute('achat') },
+    { name: 'Electrique & Hybride Rechargeable', href: '/e-brid', current: router.hasRoute('e-brid') },
+    { name: 'Services', href: '/services', current: router.hasRoute('services') },
+    { name: 'Nos concessionaires', href: '/garage', current: router.hasRoute('garage') },
+]
+</script>
+
+<template>
+    <nav class="w-full flex px-52 bg-white/10">
+        <div class="flex h-24 items-center relative justify-between w-full shadow px-2">
+            <div class="w-3/4 flex h-full">
+                <div class="min-w-[6rem] text-center group h-full" v-for="link in navLink">
+                    <div
+                        class="h-full group-hover:bg-white/20 group-hover:cursor-pointer items-center text-center justify-center flex">
+                        <a :href="link.href" class="text-white group-hover:text-blue-400 text-center">
+                            {{ link.name }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="w-1/4 flex justify-end items-center space-x-3">
+                <MagnifyingGlassIcon class="h-10 w-10 cursor-pointer" />
+                <UserIcon class="h-10 w-10 cursor-pointer" />
+            </div>
+        </div>
+        <div class="flex items-center ml-5">
+            <img src="../assets/bmw_logo.png" alt="" class="h-16 w-16">
+        </div>
+    </nav>
+</template>
--- vuejs-starter/src/views/Home.vue
+++ vuejs-starter/src/views/Home.vue
@@ -1,16 +1,15 @@
 <script setup>
-import {useStore} from "vuex";
+import { useStore } from "vuex";
+import Navbar from "../components/Navbar.vue";
 
 const store = useStore();
 </script>
 
 <template>
-    <div class="w-full h-screen">
-      <h1 class="text-green-400">Home</h1>
-      <button
-          class="bg-green-400 text-white px-4 py-2 rounded"
-          v-if="store.getters['auth/isLoggedIn']"
-          @click="store.dispatch('auth/logout')"
-      >Logout</button>
-    </div>
+  <div class="w-full h-screen">
+    <header class="h-[70vh] bg-[url('/public/bg_header.jpg')] bg-cover">
+      <Navbar />
+      <h1 class="uppercase text-3xl ml-24 mt-5 font-extrabold tracking-wider">Achat de véhicules neuf ou d'occasion</h1>
+    </header>
+  </div>
 </template>
