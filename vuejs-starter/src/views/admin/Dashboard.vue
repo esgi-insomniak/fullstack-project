968a2511b52d4e9f723059724ceb9b3688f58f67
ThePrimesBros <thesket5@outlook.fr>
2023-01-20 18:26:37 +0100

    panel admin

--- vuejs-starter/src/views/admin/Dashboard.vue
+++ vuejs-starter/src/views/admin/Dashboard.vue
@@ -1,210 +1,188 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
-    import carService from "../../services/car.service";
-    import userService from "../../services/user.service";
-    import orderService from "../../services/order.service";
+import {
+    ref,
+    onMounted
+} from 'vue';
+import carService from "../../services/car.service";
+import userService from "../../services/user.service";
+import orderService from "../../services/order.service";
 
-    const users = ref([]);
-    const voiture = ref([]);
-    const orders = ref([]);
-    const loading = ref(true);
-    const limit = ref(10);
-    const currentPage = ref(1);
+const users = ref([]);
+const voiture = ref([]);
+const orders = ref([]);
+const loading = ref(true);
+const limit = ref(10);
+// make currentPage const to be the last of the users
+const currentPage = ref()
+const orderDesc = ref(false)
+const theadUser = [{
+        id: 'id',
+        label: 'ID'
+    },
+    {
+        id: 'firstName',
+        label: 'Prenom'
+    },
+    {
+        id: 'lastName',
+        label: 'Nom'
+    },
+    {
+        id: 'email',
+        label: 'Email'
+    },
+    {
+        id: 'actions',
+        label: 'Actions'
+    },
+];
+const theadOrder = [{
+        id: 'id',
+        label: 'ID'
+    },
+    {
+        id: 'userId',
+        label: 'User Id'
+    },
+    {
+        id: 'carId',
+        label: 'Car Id'
+    },
+    {
+        id: 'date',
+        label: 'Date'
+    },
+    {
+        id: 'price',
+        label: 'Price'
+    },
+    {
+        id: 'actions',
+        label: 'Actions'
+    },
+];
+const theadCar = [{
+        id: 'id',
+        label: 'ID'
+    },
+    {
+        id: 'brand',
+        label: 'Brand'
+    },
+    {
+        id: 'model',
+        label: 'Model'
+    },
+    {
+        id: 'price',
+        label: 'Price'
+    },
+    {
+        id: 'year',
+        label: 'Year'
+    },
+    {
+        id: 'fuel',
+        label: 'Fuel'
+    },
+    {
+        id: 'kilometers',
+        label: 'Kilometers'
+    },
+    {
+        id: 'image',
+        label: 'Image'
+    },
+    {
+        id: 'actions',
+        label: 'Actions'
+    },
+];
 
-    onMounted(async () => {
-        users.value = await userService.getCollection();
-        voiture.value = await carService.getCarCollection();
-        orders.value = await orderService.getCollection();
-    });
+onMounted(async () => {
+    users.value = await userService.getCollection();
+    voiture.value = await carService.getCarCollection();
+    orders.value = await orderService.getCollection();
+});
 </script>
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Dashboard</h1>
-                <p>Here you can see all the users, cars and orders</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <h2>Users</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Role</th>
+<div class="">
+    <div>
+        <div class="w-full h-[70vh]">
+            <h2 class="text-xl">Users</h2>
+            <table class="table-auto w-full mx-auto text-left my-5">
+                <thead>
+                    <tr>
+                        <th class="py-3 px-2" v-for="th in theadUser" :key="th.id">{{ th.label }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(user, index) in users.slice(0, limit)" :key="user.id" :class="{'bg-gray-400': index % 2 === 0, 'text-black': index % 2 === 0}">
+                        <td class="py-3 px-2">{{ user.id }}</td>
+                        <td class="py-3">{{ user.firstName }}</td>
+                        <td class="py-3">{{ user.lastName }}</td>
+                        <td class="py-3">{{ user.email }}</td>
+                        <td class="py-3" :class="{'text-white': index % 2 === 0}">
+                            <button class="mr-3" @click="editUser(user.id)">Edit</button>
+                            <button class="ml-3" @click="deleteUser(user.id)">Delete</button>
+                        </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="user in users.slice(0, limit)" :key="user.id">
-                            <th scope="row">{{ user.id }}</th>
-                            <td>{{ user.email }}</td>
-                            <td>{{ user.firstName }}</td>
-                            <td>{{ user.lastName }}</td>
-                            <td>{{ user.role }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <h2>Cars</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Brand</th>
-                            <th scope="col">Model</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Year</th>
-                            <th scope="col">Fuel</th>
-                            <th scope="col">Kilometers</th>
-                            <th scope="col">Image</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="car in voiture" :key="car.id">
-                            <th scope="row">{{ car.id }}</th>
-                            <td>BMW {{ car.identity.name }}</td>
-                            <td>{{ car.identity.category.name }}</td>
-                            <td>{{ car.price }}</td>
-                            <td>{{ car.year }}</td>
-                            <td>{{ car.fuel }}</td>
-                            <td>{{ car.mileage }}</td>
-                            <td><img :src="car.images[0].src" width="150"></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <h2>Orders</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">User Id</th>
-                            <th scope="col">Car Id</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="order in orders.slice(0, limit)" :key="order.id">
-                            <th scope="row">{{ order.id }}</th>
-                            <td>{{ order.orderer.id }}</td>
-                            <td>{{ order.car.id }}</td>
-                            <td>{{ order.finalisedAt }}</td>
-                            <td>{{ order.totalPrice }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
     </div>
+    <div class="">
+        <div class="">
+            <h2 class="text-xl">Cars</h2>
+            <table class="table-auto w-full mx-auto text-left my-5">
+                <thead>
+                    <tr>
+                        <th class="py-3 px-2" v-for="th in theadCar" :key="th.id">{{ th.label }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(car, index) in voiture.slice(0, limit)" :key="car.id" :class="{'bg-gray-400': index % 2 === 0, 'text-black': index % 2 === 0}">
+                        <td class="py-3 px-2">{{ car.id }}</td>
+                        <td class="py-3">BMW {{ car.identity.name }}</td>
+                        <td class="py-3">{{ car.identity.category.name }}</td>
+                        <td class="py-3">{{ car.price }}</td>
+                        <td class="py-3">{{ car.year }}</td>
+                        <td class="py-3">{{ car.fuel }}</td>
+                        <td class="py-3">{{ car.mileage }}</td>
+                        <td class="py-3"><img :src="car.images[0].src" width="150"></td>
+                        <td class="py-3" :class="{'text-white': index % 2 === 0}">
+                            <button class="mr-3" @click="editCar(car.id)">Edit</button>
+                            <button class="ml-3" @click="deleteCar(car.id)">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="">
+        <div class="">
+            <h2 class="text-xl">Orders</h2>
+            <table class="table-auto w-full mx-auto text-left my-5">
+                <thead>
+                    <tr>
+                        <th class="py-3 px-2" v-for="th in theadOrder" :key="th.id">{{ th.label }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(order, index) in orders.slice(0, limit)" :key="order.id" :class="{'bg-gray-400': index % 2 === 0, 'text-black': index % 2 === 0}">
+                        <td class="py-3 px-2">{{ order.id }}</td>
+                        <td class="py-3">{{ order.orderer.id }}</td>
+                        <td class="py-3">{{ order.car.id }}</td>
+                        <td class="py-3">{{ order.finalisedAt }}</td>
+                        <td class="py-3">{{ order.totalPrice }}</td>
+                        <td class="py-3" :class="{'text-white': index % 2 === 0}">
+                            <button class="mr-3" @click="editOrder(order.id)">Edit</button>
+                            <button class="ml-3" @click="deleteOrder(order.id)">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 </template>
-<style>
-    .container {
-        margin-top: 50px;
-    }
-
-    .row {
-        margin-top: 50px;
-    }
-
-    .col-12 {
-        margin-top: 50px;
-    }
-
-    .table {
-        margin-top: 50px;
-    }
-
-    .table td {
-        text-align: center;
-    }
-
-    .table th {
-        text-align: center;
-    }
-
-    .table thead th {
-        border-bottom: 1px solid #dee2e6;
-    }
-
-    .table tbody + tbody {
-        border-top: 1px solid #dee2e6;
-    }
-
-    .table .table {
-        background-color: #fff;
-    }
-
-    .table-sm td,
-    .table-sm th {
-        padding: 0.3rem;
-    }
-
-    .table-bordered {
-        border: 1px solid #dee2e6;
-    }
-
-    .table-bordered td,
-    .table-bordered th {
-        border: 1px solid #dee2e6;
-    }
-
-    .table-bordered thead td,
-    .table-bordered thead th {
-        border-bottom-width: 2px;
-    }
-
-    .table-borderless tbody + tbody,
-    .table-borderless td,
-    .table-borderless th,
-    .table-borderless thead th {
-        border: 0;
-    }
-
-    .table-striped tbody tr:nth-of-type(odd) {
-        background-color: rgba(0, 0, 0, 0.05);
-    }
-
-    .table-hover tbody tr:hover {
-        color: #212529;
-        background-color: rgba(0, 0, 0, 0.075);
-    }
-
-    .table-primary,
-    .table-primary > td,
-    .table-primary > th {
-        background-color: #b8daff;
-    }
-
-    .table-primary tbody + tbody,
-    .table-primary td,
-    .table-primary th,
-    .table-primary thead th {
-        border-color: #7abaff;
-    }
-
-    .table-hover .table-primary:hover {
-        background-color: #9fcdff;
-    }
-
-    .table-hover .table-primary:hover > td,
-    .table-hover .table-primary:hover > th {
-        background-color: #9fcdff;
-    }
-
-    .table-secondary,
-    .table-secondary > td,
-    .table-secondary > th {
-        background-color: #d6d8db;
-    }
-</style>
