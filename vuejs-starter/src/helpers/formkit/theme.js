a5aaa4b28c7837b9c673aa3be6e2c8ac16d3b40e
Loan CLERIS <loan.cleris@gmail.com>
2023-02-08 15:40:34 +0100

    Change form html to FormKit
    - wip: on end of form, send to api and on response generate QrCode for DL Ical or googleCal

--- vuejs-starter/src/helpers/formkit/theme.js
+++ vuejs-starter/src/helpers/formkit/theme.js
@@ -47,7 +47,7 @@ export default {
     radio: {
         outer: 'mb-5',
         label: 'block mb-1 font-bold text-sm',
-        inner: 'border border-gray-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+        inner: 'border border-gray-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500 flex items-center p-2',
         input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
         help: 'text-xs text-gray-500',
         messages: 'list-none p-0 mt-1 mb-0',
--- vuejs-starter/src/views/Services.vue
+++ vuejs-starter/src/views/Services.vue
@@ -1,17 +1,16 @@
 <script setup>
 import { ref } from 'vue'
-import RadioGroup from '../components/RadioGroup.vue'
 const questions = [
     {
-        name: 'Pour quelle raison souhaitez vous prendre RDV ?', type: 'radio', options: [
+        label: 'Pour quelle raison souhaitez vous prendre RDV ?', type: 'radio', options: [
             { label: 'Révision', value: 'revision', id: 1 },
             { label: 'Réparation', value: 'reparation', id: 2 },
             { label: 'Vidange', value: 'vidange', id: 3 },
             { label: 'Autre', value: 'autre', id: 4 },
-        ], step: 1
+        ], step: 1, name: 'reason'
     },
     {
-        name: 'Quel est le modèle de votre véhicule ?', type: 'select', step: 2, options: [
+        label: 'Quel est le modèle de votre véhicule ?', type: 'select', step: 2, options: [
             { label: 'Audi', value: 'audi' },
             { label: 'BMW', value: 'bmw' },
             { label: 'Citroen', value: 'citroen' },
@@ -34,25 +33,25 @@ const questions = [
             { label: 'Toyota', value: 'toyota' },
             { label: 'Volkswagen', value: 'volkswagen' },
             { label: 'Volvo', value: 'volvo' },
-        ]
+        ], name: 'model'
     },
-    { name: 'Quel est le kilométrage de votre véhicule ?', type: 'number', step: 2 },
+    { label: 'Quel est le kilométrage de votre véhicule ?', type: 'number', step: 3, name: 'kilometrage' },
     {
-        name: 'Quel est le type de carburant de votre véhicule ?', type: 'radio', options: [
+        label: 'Quel est le type de carburant de votre véhicule ?', type: 'radio', options: [
             { label: 'Essence', value: 'essence', id: 1 },
             { label: 'Diesel', value: 'diesel', id: 2 },
             { label: 'Hybride', value: 'hybride', id: 5 },
             { label: 'Electrique', value: 'electrique', id: 6 },
-        ], step: 2
+        ], step: 4, name: 'fuel'
     },
     {
-        name: 'Quel est le type de boite de vitesse de votre véhicule ?', type: 'radio', options: [
+        label: 'Quel est le type de boite de vitesse de votre véhicule ?', type: 'radio', options: [
             { label: 'Manuelle', value: 'manuelle', id: 1 },
             { label: 'Automatique', value: 'automatique', id: 2 },
-        ], step: 3
+        ], step: 5, name: 'gearbox'
     },
     {
-        name: 'Quel est le type de carrosserie de votre véhicule ?', type: 'select', options: [
+        label: 'Quel est le type de carrosserie de votre véhicule ?', type: 'select', options: [
             { label: 'Berline', value: 'berline' },
             { label: 'Break', value: 'break' },
             { label: 'Coupé', value: 'coupe' },
@@ -60,14 +59,14 @@ const questions = [
             { label: '4x4', value: '4x4' },
             { label: 'Monospace', value: 'monospace' },
             { label: 'Utilitaire', value: 'utilitaire' },
-        ], step: 4
+        ], step: 6, name: 'body'
     },
-    { name: 'Selectionnez un créneau qui vous convient:', type: 'datetime', step: 5 }
+    { label: 'Selectionnez un créneau qui vous convient:', type: 'datetime-local', step: 7, name: 'appointement' }
 ]
 const answers = ref([])
-const currentQuestion = ref(0)
-const nextQuestion = (e) => {
-    e.preventDefault()
+const currentQuestion = ref(1)
+const nextQuestion = (userChoice) => {
+    answers.value.push(userChoice)
     currentQuestion.value++
 }
 const previousQuestion = (e) => {
@@ -75,43 +74,25 @@ const previousQuestion = (e) => {
     currentQuestion.value--
 }
 
+console.log(answers.value)
+
 </script>
 
-
 <template>
-    <form class="p-6 rounded-lg">
-        <template v-for="(question, index) in questions" :key="index">
-            <div v-if="currentQuestion === index">
-                <label class="block text-white font-medium">{{ question.name }}</label>
-                <div v-if="question.type === 'radio'">
-                    <RadioGroup :options="question.options" flex-direction="flex-col" />
-                </div>
-                <div v-if="question.type === 'select'">
-                    <select
-                        class="block p-3 w-full mt-1 border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-none">
-                        <option v-for="option in question.options" :key="option.value" :value="option.value">
-                            {{ option.label }}
-                        </option>
-                    </select>
-                </div>
-                <div v-if="question.type === 'number'">
-                    <input type="number"
-                        class="block p-3 w-full mt-1 border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-none">
-                </div>
-                <div v-if="question.type === 'datetime'">
-                    <input type="datetime-local"
-                        class="block p-3 w-full mt-1 border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-none">
-                </div>
-            </div>
-        </template>
-        <div class="text-center mt-6">
-            <button v-if="currentQuestion > 0" @click="previousQuestion"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md">Previous</button>
-            <button v-if="currentQuestion < questions.length - 1" @click="nextQuestion"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md">Next</button>
-            <pre>{{ currentQuestion }}</pre>
-            <button v-if="currentQuestion === questions.length - 1" type="submit"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md">Submit</button>
+    <div class="h-[88vh] w-full py-10">
+        <div class="flex flex-col px-64 h-full overflow-scroll" v-if="currentQuestion !== questions.lenght + 1">
+            <FormKit type="form" @submit="nextQuestion"
+                :submit-label="currentQuestion === questions.length ? 'Demande de RDV' : 'Question suivante'">
+                <template v-for="question in questions">
+                    <FormKit :type="question.type" :name="question.name" :label="question.label"
+                        :options="question.options" v-if="question.step === currentQuestion" />
+                </template>
+            </FormKit>
         </div>
-    </form>
+        <div v-show="currentQuestion === questions.length + 1">
+            Questionnaire terminer
+
+        </div>
+    </div>
+
 </template>
