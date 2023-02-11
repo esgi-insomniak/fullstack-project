0240e6407e70d7f5c4a5fcf01dc27cc03863d477
Tom Cuillandre <tcuillandre@myges.fr>
2023-02-11 16:32:53 +0100

    Fix Consumer service request cypress

--- vuejs-starter/cypress/e2e/ConsumerServiceRequest.cy.js
+++ vuejs-starter/cypress/e2e/ConsumerServiceRequest.cy.js
@@ -10,9 +10,10 @@ describe('template spec', () => {
       cy.get('ul[class="formkit-options"]').children().eq(random).click()
     })
     cy.get('button').contains('Question suivante').click()
-    cy.get('select').children().then($childrens => {
+    cy.get('select[name="model"]').children().should('have.length.gt', 2)
+    cy.get('select[name="model"]').children().then($childrens => {
       const random = Math.floor(Math.random() * $childrens.length)
-      cy.get('select').select($childrens.eq(random).val())
+      cy.get('select[name="model"]').select($childrens.eq(random).val())
     })
     cy.get('button').contains('Question suivante').click()
     cy.get('input[name="kilometrage"]').type(Math.floor(Math.random() * (100000 - 5000) + 5000))
@@ -27,14 +28,16 @@ describe('template spec', () => {
       cy.get('ul[class="formkit-options"]').children().eq(random).click()
     })
     cy.get('button').contains('Question suivante').click()
-    cy.get('select').children().then($childrens => {
+    cy.get('select[name="concession"]').children().should('have.length.gt', 2)
+    cy.get('select[name="concession"]').children().then($childrens => {
       const random = Math.floor(Math.random() * $childrens.length)
-      cy.get('select').select($childrens.eq(random).val())
+      cy.get('select[name="concession"]').select($childrens.eq(random).val())
     })
     cy.get('button').contains('Question suivante').click()
-    cy.get('select').children().then($childrens => {
+    cy.get('select[name="appointement"]').children().should('have.length.gt', 2)
+    cy.get('select[name="appointement"]').children().then($childrens => {
       const random = Math.floor(Math.random() * ($childrens.length - 1)) + 1
-      cy.get('select').select($childrens.eq(random).val())
+      cy.get('select[name="appointement"]').select($childrens.eq(random).val())
     })
     cy.get('button').contains('Demande de RDV').click()
     cy.get('div').children().contains('Questionnaire terminer').should('be.visible')
